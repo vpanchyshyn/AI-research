f59f4c236ea05ff69e66fe75b3aac7f8953e7ed6
gruvert <147984995+gruvert@users.noreply.github.com>
2024-04-26 21:14:12 +0300

    Created first post test module line

--- week-3/Line/post_test_change_line.py
+++ week-3/Line/post_test_change_line.py
@@ -1,52 +1,34 @@
+"""intersections"""
 class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def __eq__(self, other):
-        if isinstance(other, Point):
-            return self.x == other.x and self.y == other.y
-        return False
-
+    """point object"""
+    def __init__(self, x, y) -> None:
+        self.x = float(x)
+        self.y = float(y)
+    def __eq__(self, other) -> bool:
+        return self.x == other.x and self.y == other.y
 class Line:
-    def __init__(self, point1, point2):
-        if not isinstance(point1, Point) or not isinstance(point2, Point):
-            raise ValueError('Параметри мають бути об\'єктами класу Point')
+    """line object"""
+    def __init__(self, point1, point2) -> None:
         self.point1 = point1
         self.point2 = point2
-        if point1 == point2:
-            raise Exception
-        try:
-            self.k, self.b = self.calculate_coefficients()
-        except ZeroDivisionError:
-            self.k, self.b = float('inf'), float('inf')  # Vertical line case
 
-    def calculate_coefficients(self):
-        x1, y1 = self.point1.x, self.point1.y
-        x2, y2 = self.point2.x, self.point2.y
-        if x2 - x1 == 0:
-            raise ZeroDivisionError('Лінія вертикальна')
-        k = (y2 - y1) / (x2 - x1)
-        b = y1 - k * x1
-        return k, b
+    def intersect(self, other):
+        """check if two lines intersect"""
+        sub_1 = self.point2.y - self.point1.y
+        sub_rev_1 = self.point1.x - self.point2.x
+        sum_1 = sub_1 * (self.point1.x) + sub_rev_1 * (self.point1.y)
+        sub_2 = other.point2.y - other.point1.y
+        sub_rev_2 = other.point1.x - other.point2.x
+        sum_2 = sub_2 * (other.point1.x) + sub_rev_2 * (other.point1.y)
 
-    def intersect(self, line):
-        try:
-            k_self, b_self = self.calculate_coefficients()
-            k_line, b_line = line.calculate_coefficients()
-        except ZeroDivisionError:
-            if self.point1.x == line.point1.x:  # Check if lines are vertically aligned
-                if self.point1 == line.point1 or self.point1 == line.point2:
-                    return self.point1  # Return common endpoint
-                elif self.point2 == line.point1 or self.point2 == line.point2:
-                    return self.point2  # Return common endpoint
-                else:
-                    return None  # No common points for parallel vertical lines
-            return None  # No intersection for parallel non-vertical lines
-        if k_self == k_line and b_self == b_line:
-            return self  # Lines are identical
-        if k_self == k_line:
-            return None  # Lines are parallel and non-identical
-        x = (b_line - b_self) / (k_self - k_line)
-        y = k_self * x + b_self
-        return Point(x, y)  # Intersection point
+        dit = sub_1 * sub_rev_2 - sub_2 * sub_rev_1
+
+        if dit == 0:
+            if sub_1 * other.point1.x + sub_rev_1 * other.point1.y == sum_1 and \
+                    sub_1 * other.point2.x + sub_rev_1 * other.point2.y == sum_1:
+                return self.point1
+            return None
+
+        x = (sub_rev_2 * sum_1 - sub_rev_1 * sum_2) / dit
+        y = (sub_1 * sum_2 - sub_2 * sum_1) / dit
+        return Point(x, y)
