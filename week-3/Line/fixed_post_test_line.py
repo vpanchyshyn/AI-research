a73318d094fcfaea2756aca59936841d886bd692
gruvert <147984995+gruvert@users.noreply.github.com>
2024-04-26 21:23:42 +0300

    Created fixed_post_test_line.py with small fixes of first post test line module

---
+++ week-3/Line/fixed_post_test_line.py
@@ -0,0 +1,34 @@
+"""intersections"""
+class Point:
+    """point object"""
+    def __init__(self, x, y) -> None:
+        self.x = float(x)
+        self.y = float(y)
+    def __eq__(self, other) -> bool:
+        return self.x == other.x and self.y == other.y
+class Line:
+    """line object"""
+    def __init__(self, point1, point2) -> None:
+        self.point1 = point1
+        self.point2 = point2
+
+    def intersect(self, other):
+        """check if two lines intersect"""
+        sub_1 = self.point2.y - self.point1.y
+        sub_rev_1 = self.point1.x - self.point2.x
+        sum_1 = sub_1 * (self.point1.x) + sub_rev_1 * (self.point1.y)
+        sub_2 = other.point2.y - other.point1.y
+        sub_rev_2 = other.point1.x - other.point2.x
+        sum_2 = sub_2 * (other.point1.x) + sub_rev_2 * (other.point1.y)
+
+        dit = sub_1 * sub_rev_2 - sub_2 * sub_rev_1
+
+        if dit == 0:
+            if sub_1 * other.point1.x + sub_rev_1 * other.point1.y == sum_1 and \
+                    sub_1 * other.point2.x + sub_rev_1 * other.point2.y == sum_1:
+                return self
+            return None
+
+        x = (sub_rev_2 * sum_1 - sub_rev_1 * sum_2) / dit
+        y = (sub_1 * sum_2 - sub_2 * sum_1) / dit
+        return Point(x, y)
